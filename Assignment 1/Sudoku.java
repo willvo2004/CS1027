593b2bf379f6629409369b80e8f59a3ce9e9d28c
willvo2004 <willvo2004@gmail.com>
2023-09-18 15:03:59 -0400

    Added classes to assignment 1

---
+++ Assignment 1/Sudoku.java
@@ -0,0 +1,46 @@
+public class Sudoku {
+
+    private int size;
+    private int[][] grid;
+
+    public Sudoku (int[][] numbers) {
+
+    }
+
+    public int getSize () {
+
+    }
+
+    public int[][] getGrid () {
+
+    }
+
+    public int getDigitAt (int row, int col) {
+
+    }
+
+    public boolean isValidRow (int row) {
+
+    }
+
+    public boolean isValidCol (int col) {
+
+    }
+
+    public boolean isValidBox (int row, int col) {
+
+    }
+
+    public boolean isValidSolution () {
+
+    }
+
+    public boolean equals (Sudoku other) {
+
+    }
+
+    public String toString () {
+
+    }
+
+}
