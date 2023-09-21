0ff57ac59556a85e980467d78fd73b185c5895e3
willvo2004 <willvo2004@gmail.com>
2023-09-21 19:06:56 -0400

    Added Lab 1

--- Labs/Lab1/src/Player.java
+++ Labs/Lab1/src/Player.java
@@ -1,3 +1,67 @@
+/**
+ * This class represents a hockey player. Each hockey player has a name, a position, and a jersey number
+ * @author william vo
+ */
 public class Player {
 
+    private String name;
+    private String position;
+    private int jerseyNum;
+
+    /**
+     * Constructor creates player object with a name, position, and jersey number
+     * @param name name of the player
+     * @param position position of the player
+     * @param jerseyNum jersey number of the player
+     */
+    public Player(String name, String position, int jerseyNum) {
+        this.name = name;
+        this.position = position;
+        this.jerseyNum = jerseyNum;
+    }
+
+    /**
+     * Accessor method to get the name of a hockey player
+     * @return name of hockey player
+     */
+    public String getName() {
+        // Get the player's name.
+        return name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public int getJerseyNum() {
+        return jerseyNum;
+    }
+
+    /**
+     * Setter method to allow user to set a new name for a hockey player
+     * @param newName new name for the hockey player
+     */
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    public void setPosition(String newPosition) {
+        this.position = newPosition;
+    }
+
+    public void setJerseyNum(int newJerseyNum) {
+        this.jerseyNum = newJerseyNum;
+    }
+
+    public String toString() {
+        return this.name + ": #" + this.jerseyNum;
+    }
+
+    public boolean equals(Player other) {
+        if (this.name.equals(other.name) && this.jerseyNum == other.jerseyNum) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
--- Labs/Lab1/src/TestLab.java
+++ Labs/Lab1/src/TestLab.java
@@ -1,5 +1,27 @@
 public class TestLab {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Player p1 = new Player("Will", "GK", 1);
+
+        System.out.println(p1.getName());
+        System.out.println(p1.getPosition());
+        System.out.println(p1.getJerseyNum());
+
+        p1.setName("Wayne Gretsky");
+        p1.setPosition("Center");
+        p1.setJerseyNum(87);
+
+        System.out.println(p1.getName());
+        System.out.println(p1.getPosition());
+        System.out.println(p1.getJerseyNum());
+
+        System.out.println(p1);
+
+        Player p2 = new Player("Wayne Gretsky", "Center", 87);
+
+        if (p1.equals(p2)) {
+            System.out.println("Same player");
+        } else {
+            System.out.println("Different player");
+        }
     }
 }
