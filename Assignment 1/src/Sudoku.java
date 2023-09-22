b2dc8a50b22dc15a14e716e15e078601c610c64b
willvo2004 <willvo2004@gmail.com>
2023-09-22 12:09:20 -0400

    Added methods to Sudoku class

--- Assignment 1/src/Sudoku.java
+++ Assignment 1/src/Sudoku.java
@@ -4,19 +4,25 @@ public class Sudoku {
     private int[][] grid;
 
     public Sudoku (int[][] numbers) {
-
+        this.size = numbers.length; // grid is always a square, length of 2D array will equal length of element array
+        this.grid = numbers; // shallow copy of numbers
     }
 
     public int getSize () {
-
+        return this.size;
     }
 
     public int[][] getGrid () {
-
+        return this.grid;
     }
 
     public int getDigitAt (int row, int col) {
-
+        // try-catch block handle exception when class compiles
+        try {
+            return this.grid[row][col];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return -1;
+        }
     }
 
     public boolean isValidRow (int row) {
--- Assignment 1/src/UniqueDiagonalSudoku.java
+++ Assignment 1/src/UniqueDiagonalSudoku.java
@@ -1,4 +1,4 @@
-public class UniqueDiagonalSudoku {
+public class UniqueDiagonalSudoku extends Sudoku {
 
 
 }
