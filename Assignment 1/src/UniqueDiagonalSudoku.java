6caaf9496b83ebe18be3019f2af0e12fcd6b991d
willvo2004 <willvo2004@gmail.com>
2023-10-03 18:12:53 -0400

    Completed Assignment 1

--- Assignment 1/src/UniqueDiagonalSudoku.java
+++ Assignment 1/src/UniqueDiagonalSudoku.java
@@ -2,21 +2,12 @@ public class UniqueDiagonalSudoku extends Sudoku {
     public UniqueDiagonalSudoku (int[][] numbers) {
         super(numbers);
     }
-    // {1*, 0, 0, 0, 0, 0, 0, 0, 9*},   top left - bottom right
-    // {0, 5*, 0, 0, 0, 0, 0, 2*, 0},   [0][0] [1][1] [2][2] [3][3] [4][4] [5][5] [6][6] [7][7] [8][8]
-    // {0, 0, 9*, 0, 0, 0, 4*, 5, 0},
-    // {0, 0, 0, 5*, 0, 7*, 8, 9, 0},   bottom left - top right
-    // {0, 0, 0, 0, 9*, 0, 0, 0, 0},    [8][0] [7][1] [6][2] [5][3] [4][4] [3][5] [2][6] [1][7] [0][8]
-    // {0, 0, 0, 2*, 0, 4*, 0, 0, 0},
-    // {0, 0, 5*, 0, 0, 0, 9*, 0, 0},
-    // {0, 7*, 0, 0, 0, 0, 0, 4*, 0},
-    // {9*, 0, 0, 0, 0, 0, 0, 0, 8*}
-
     /**
      * In addition to checking if the entire grid abides by the normal rules of Sudoku, it will
      * also check if the main diagonals of the grid abide by the same rules. A valid diagonal
      * has no duplicates and is only between 1 and size n.
-     * @return True someshit
+     * @return True if every row, column, 3x3 box in a 9x9 grid, and if one or both diagonals are
+     * valid. False if otherwise.
      */
     @Override
     public boolean isValidSolution() {
