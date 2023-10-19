80536bebc802c1580cc165e4ce9ac26b1fbc4410
willvo2004 <willvo2004@gmail.com>
2023-10-19 01:00:22 -0400

    Changed assignment 2

--- Assignment 2/src/OrderedLinkedList.java
+++ Assignment 2/src/OrderedLinkedList.java
@@ -12,28 +12,25 @@ public class OrderedLinkedList<T extends Comparable<T>> {
         Node<T> newNode = new Node<>(newData, null);
 
         if (head == null) { // handles null pointer exception
-            head = newNode; //
+            head = newNode;
+            size += 1;
             return;
         }
         Node<T> curr = head;
         Node<T> prev = null;
 
-        while (curr.getNext() != null && newData.compareTo(curr.getData()) <= 0) {
+        while (curr != null && newData.compareTo(curr.getData()) <= 0) {
             prev = curr;
-            curr = curr.getNext();
+            curr = curr.getNext(); // would this not throw a null pointer exception?
         }
-        if (curr.getNext() == null) { // if newData is the smallest
-            curr.setNext(newNode);
+        if (prev == null) {
+            newNode.setNext(curr); // argument can curr or head? Because of line 19?
+            head = newNode;
+        } else {
+            newNode.setNext(curr);
+            prev.setNext(newNode);
         }
-        else {
-            if (prev == null) {
-                newNode.setNext(curr);
-            } else {
-                prev.setNext(newNode);
-                newNode.setNext(curr);
-            }
-
-        }
+        size += 1;
     }
 
     public T get(int i) {
@@ -55,20 +52,7 @@ public class OrderedLinkedList<T extends Comparable<T>> {
         throw new IndexOutOfBoundsException();
     }
     public int getSize() {
-        Node<T> curr = head;
-        int counter = 0;
-
-        while (curr != null) {
-            counter++;
-            if (curr.getNext() == null) {
-                return counter;
-            }
-            else {
-                curr = curr.getNext();
-            }
-        }
-
-        return counter;
+        return size;
     }
 
 }
--- Assignment 2/src/myOLLTest.java
+++ Assignment 2/src/myOLLTest.java
@@ -3,22 +3,27 @@ public class myOLLTest {
 
         OrderedLinkedList<Integer> oll = new OrderedLinkedList<>();
 
+        //int x = 1;
+        //int y = 1;
         int x = (int) (Math.random()*100);
         int y = (int) (Math.random()*100);
         int[] all = new int[] {x,y};
+        int first = 0;
+        int second = 0;
 
-        oll.insert(x);
+        oll.insert(x); // insert the first number
         oll.insert(y); // order is not happening
 
         if (oll.getSize()!= 2) {
             System.out.println(false);
+            System.out.println(oll.getSize());
         } else {
-            int first = oll.get(0);
-            int second = oll.get(1);
-
-            System.out.println(first);
-            System.out.println(second);
+            first = oll.get(0);
+            second = oll.get(1);
         }
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(oll.getSize());
     }
 
     public static int test02() {
