6ded252627610a74d378dbe257661c7244b48701
willvo2004 <willvo2004@gmail.com>
2023-11-07 02:40:54 -0500

    findNextCell Method and new test file for Assignment 3

--- Assignment 3/src/MineEscape.java
+++ Assignment 3/src/MineEscape.java
@@ -2,7 +2,7 @@ public class MineEscape {
 
     private Map map;
     private int numGold;
-    private int[] numKeys;
+    private int[] numKeys; // [red, green, blue]
 
     public MineEscape(String filename) {
         try {
@@ -16,13 +16,53 @@ public class MineEscape {
     }
 
     public MapCell findNextCell (MapCell cell) {
+        MapCell[] cellNeighbors = new MapCell[4];
         for (int i = 0; i < 4; i ++) {
-            if (cell.getNeighbour(i) != null) {
-                if (walkableCellCheck(cell)) {
+            if (walkableCellCheck(cell)) {cellNeighbors[i] = cell.getNeighbour(i);}  // add cells to an array
+        }
 
+        int collectableCells = 0;
+        int floorCells = 0;
+        int lockCells = 0;
+        for (int i = 0; i < cellNeighbors.length; i++) {
+            if (cellNeighbors[i] != null) {
+
+                if (cellNeighbors[i].isExit()) {
+                    return cellNeighbors[i];
+                }
+                else if (cellNeighbors[i].isGoldCell() || cellNeighbors[i].isKeyCell()) {
+                    collectableCells ++;
+                }
+                else if (cellNeighbors[i].isFloor()) {
+                    floorCells ++;
+                }
+                else if (cellNeighbors[i].isLockCell()) {
+                    lockCells ++;
                 }
             }
         }
+        if (collectableCells >= 1) {
+            for (int i = 0; i < cellNeighbors.length; i++) {
+                if (cellNeighbors[i] != null) {
+                    if (cellNeighbors[i].isGoldCell() || cellNeighbors[i].isKeyCell()) {
+                        return cellNeighbors[i]; // should return the lowest index?
+                    }
+                }
+            }
+        }
+        else if (floorCells >= 1) {
+            for (int i = 0; i < cellNeighbors.length; i++) {
+                if (cellNeighbors[i] != null) {
+                    if (cellNeighbors[i].isFloor()) {
+                        return cellNeighbors[i]; // should return the lowest index?
+                    }
+                }
+            }
+        }
+        else if (lockCells >= 1) {
+            // do i change attribute values in this method? (Do I keep track of gold and keys?)
+        }
+
         return null;
     }
 
---
+++ Assignment 3/src/MyPathTest.java
@@ -0,0 +1,8 @@
+public class MyPathTest {
+
+    public static void main(String[] args) {
+        MapCell[] cellArray = new MapCell[3];
+
+        System.out.println(cellArray.length);
+    }
+}
